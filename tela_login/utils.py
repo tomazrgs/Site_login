c7b53a35d476841ff519690a63d92c76dd72db9a
Tomaz Roberto <tomazgoncalvessantos@gmail.com>
2025-11-26 19:58:31 -0300

    substitui o arquivo

---
+++ tela_login/utils.py
@@ -0,0 +1,4 @@
+pessoas = [{'login':'indeed','senha': '1234', 'adm': False},
+           {'login':'fernanda0','senha': 'africa', 'adm': False}]
+
+adm = [{'login': 'adm', 'senha': 'adm123', 'adm': True}]
