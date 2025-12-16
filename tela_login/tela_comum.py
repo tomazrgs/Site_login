097a66f20f55dc49f78c840f502b10d9fe1c1664
Tomaz Roberto <69155134+tomazrgs@users.noreply.github.com>
2025-12-16 07:10:46 -0300

    Arrumado passagem arquivo por json
    Arquivo que estava no computador do trabalho que eu não havia passado para o git

--- tela_login/tela_comum.py
+++ tela_login/tela_comum.py
@@ -1,111 +1,101 @@
-from dados import pessoas
-from dados import adm
+import time
+import json
 
-from utils import criptografa_lista
 from utils import criptografa_senha
 from utils import limpar_tela
 from utils import voltar_menu
 from utils import fechar
 from utils import listar_usuarios
-from utils import copiar_json
+from utils import remove_espaco
 from utils import leitura_json
+from utils import copiar_json
+from utils import procura_caminho
 
 def entrar():
-    limpar_tela()
-     
-    i = 0
+    x = 0
 
-    #/ .lower() força todas os caracteres do input a ficarem minusculos.
+    while x < 3:
+        usuario_encontrado = False
+        dados = leitura_json('dados.json')
 
-    usuario = input('Informe seu usuario: ').lower() 
+        usuario = input('Informe seu usuario: ').lower() 
+        usuario = remove_espaco(usuario)
 
-    #/ Valida se o nome digitado a cima realmente existe dentro do dicionário pessoas.
-    login_encontrado = None
+        while usuario == '':
+            usuario = input('Escreva nome de usuario: ')
+            usuario = remove_espaco(usuario)
 
-    for pessoa in pessoas:
-        if pessoa['login'] == usuario:
-            login_encontrado = pessoa #/ aqui, foi usado if para armazenar a posição do usuario desejado, para quando for comparar a senha ele compare a senha do usuario correto.
-            break
-        
-    #/Valida se a senha digitada realmente condiz com a senha exata do usuario na lista.
-    #/Caso senha ou usuario estejam errados, exibira uma mensagem e puxara o menu novamente 
-
-    if login_encontrado == None:
-        input('Usuario não encontrado, tente novamente')
-        voltar_menu()
-
-    senha = input(f'Olá {usuario} informe sua senha: ')
-
-    senha_criptografada = criptografa_senha(senha)
-
-    if login_encontrado['senha'] ==  senha_criptografada:
-        limpar_tela()
-        print('----LOGADO COM SUCESSO----')
-
-        if login_encontrado['adm']:
-            print('--bem vindo ao menu de adm--')
-
-    elif  login_encontrado['senha'] !=  senha_criptografada and i < 2:
-        senha = input('Usuario ou senha invalida, tente novamente:')
+        senha = input(f'Olá {usuario} informe sua senha: ')
         senha_criptografada = criptografa_senha(senha)
-        i = i+i
-        
-    else: voltar_menu(menu)
+
+        for dado in dados:
+            if dado['login'] == usuario and dado['senha'] == senha_criptografada:
+                usuario_encontrado = True
+                print('logado com sucesso')
+                break
+
+        if not usuario_encontrado:
+            x += 1
+            print('Usuario ou senha invalidos, por favor tente novamente:')
+            time.sleep(1)
+            
+
+    if x == 3:
+        print('Limite de tentativas excedidas, tente novamente.')
+        time.sleep(2)
+        voltar_menu(menu)
 
 def cadastrar():
+    dados = leitura_json('dados.json')
     copiar_json('dados.json', 'backup.json')
-    i = 0 #/ iniciamos a variavel i para utiliza-la em uma validação a baixo, sempre que for incrementar uma variavel é necessário inicialização, por ser int não podemos utilizar None.
+
+    i = 0 
 
     usuario = input('Escolha um nome de usuario: \n')
-    usuario = usuario.replace(' ','') #/ Troca espaços por areas vazias para a validação a baixo.
+    usuario = remove_espaco(usuario)
+    endereco = procura_caminho('dados.json')
 
-    while usuario == '':
-        print('Usuario invalido, por favor informe um usuario DECENTE \n')
-        usuario = input('Informe outro usuario: ')
-        usuario = usuario.replace(' ','') #/ Troca espaços por areas vazias.
-
-    #/ Compara o usuario digitado no dicionário de pessoas, para não existir usuários iguais.
-    #/ Tem um limite de 3 (tres) tentativas, caso permaneça colocando usuarios existentes, sera levado ao menu.
-    #/ O problema em adicionar limite de tentativas foi resolvido com o primeiro IF, apenas pedindo para incrementar i.
-
-    for pessoa in pessoas:
-        if pessoa['login'] == usuario:
-            print('Este usuário já existe por favor informe outro: ')
+    for dado in dados:
+        while dado['login'] == usuario or usuario == '':
+            usuario = input('Usuario invalido, por favor tente outro: ')
             i += 1
             if i == 3:
-                input('Tentativas excedidas, por favor tente novamente do menu')
+                print('Tentativas excedidas, por favor tente novamente do menu')
+                time.sleep(2)
                 voltar_menu(menu)
 
-    #/ Ao solicitar a criação da senha, tive um desafio, como impedir que o usuario coloque apenas espaço ou apenas de ENTER no input.
+    senha = input('Insira a senha: ')
+    senha = remove_espaco(senha) 
 
-    senha = input('Crie uma senha: ')
-    senha = senha.replace(' ','') #/ O desafio foi resolvido fácilmente utilizando replace, um tempo depois adicionei replace a cina para validar o usuario também.
-
-    while senha == '': #/ Utilizei while para que 'enquanto a senha tiver apenas enter ou espaço', ele entrara no laço e pedira uma senha até ser da forma correta.
+    while senha == '': 
 
         print('Senha invalida por favor tente novamente\n')
         senha = input('Crie uma senha: ')
-        senha = senha.replace(' ','')
-
-    #/ Criptografa a senha utilizando MD5 e formata logo em baixo para ser exibida apenas em exadecimal.
+        senha = remove_espaco(senha)
 
     senha_criptografada = criptografa_senha(senha)
 
-    #/ Solicitamos para que digite novamente a senha, exatamente como foi digitado a cima, para validação.
-
     repet_senha = input('Digite a senha novamente:')
-    repet_senha = repet_senha.replace(' ','')
+    repet_senha = remove_espaco(senha)
 
-    while repet_senha != senha: #/ caso seja diferente, peço para redigitar, pensando em adicionar limite de tentativas.
+    y =0
 
+    while repet_senha != senha and y < 3:
+        y += 1
         repet_senha = input('As senhas são diferentes, por favor digite a mesma senha:')
-        repet_senha = repet_senha.replace(' ','')
-
-    #/ Adiciono o usuario novo ao dicionário e exibo uma mensagem de cadastro bem sucedido.
+        repet_senha = remove_espaco(senha)
+    if y == 3:
+        print('tentativas excedidas, por favor tente novamente')
+        time.sleep(2)
 
     novo_usuario = {'login': usuario , 'senha': senha_criptografada, 'adm': False}
-    pessoas.append(novo_usuario)
+
+    with open(endereco, 'w', encoding='utf-8') as file:
+        dados.append(novo_usuario)
+        json.dump(dados, file, indent=4)
+
     print(f'{usuario} seu cadastro foi feito com sucesso!! ')
+    time.sleep(2)
     voltar_menu(menu)
 
 def menu():
@@ -134,5 +124,4 @@ def opcoes():
             fechar()
 
 if __name__ == '__main__':
-    criptografa_lista()
     menu()
