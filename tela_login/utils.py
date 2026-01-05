a87a8e673516536b4c6bfefd12af0d37beb23736
Tomaz Roberto <69155134+tomazrgs@users.noreply.github.com>
2026-01-05 11:06:50 -0300

    Update utils.py
    Arrumado bugs com json, modificado função de trocar senha para incorporar verificação de pergunta secreta e criado nova função de reset de senha para os adms.
    
    Função especifica para validar resposta da pergunta secreta

--- tela_login/utils.py
+++ tela_login/utils.py
@@ -3,10 +3,68 @@ import hashlib
 import json
 import time
 
+def trocar_senha_adm():
+    copiar_json('dados.json', 'backup.json')
+    dados = leitura_json('dados.json')
+    endereco = procura_caminho('dados.json')
 
+    senha_trocada = False
+
+    usuario = input('informe o nome de usuario: ')
+
+    for dado in dados:
+        if usuario == dado['login']:
+            nova_senha = input('Digite a nova senha: ')
+            nova_senha = remove_espaco(nova_senha)
+            senha_criptografada = criptografa(nova_senha)
+            dado['senha'] = senha_criptografada
+            senha_trocada = True
+
+    if  senha_trocada:
+        with open(endereco, 'w', encoding= 'utf-8') as arquivo:
+            json.dump(dados, arquivo, indent=4)
+        print('senha alterada com sucesso')
+    else: print('Insira um usuario existente')
+
+def trocar_senha_validacao():
+    copiar_json('dados.json', 'backup.json')
+    dados = leitura_json('dados.json')
+    endereco = procura_caminho('dados.json')
+
+    senha_trocada = False
+
+    usuario = input('informe o nome de usuario: ')
+    
+    valida_pergunta = valida_resposta(usuario)
+
+    if valida_pergunta:
+        for dado in dados:
+            if usuario == dado['login']:
+                nova_senha = input('Digite a nova senha: ')
+                nova_senha = remove_espaco(nova_senha)
+                senha_criptografada = criptografa(nova_senha)
+                dado['senha'] = senha_criptografada
+                senha_trocada = True
+
+        if  senha_trocada:
+            with open(endereco, 'w', encoding= 'utf-8') as arquivo:
+                json.dump(dados, arquivo, indent=4)
+            print('Senha alterada com sucesso')
+
+    else: print('Insira um usuario existente')
+
+def valida_resposta(usuario):
+    dados = leitura_json('dados.json')
+    for dado in dados:
+        if usuario == dado['login']:
+            resposta = input(dado['pergunta'])
+            if resposta == dado['resposta']:
+                return True
+            else: return False
+    
 def remove_espaco(string):
-    string = string.replace(' ','')
-    return string
+    string_correta = string.replace(' ','')
+    return string_correta
 
 def procura_caminho(nome_arquivo):
     caminho = os.path.join(os.path.dirname(__file__), nome_arquivo)
@@ -25,13 +83,36 @@ def copiar_json(nome_arquivo, nome_backup):
         with open(caminho_backup, 'w', encoding = 'utf-8') as backup:
                 json.dump(dados, backup, indent=4)
 
-def criptografa_senha(senha):
+def criptografa(senha):
 
     #/ Chamada quando necessario criptografar uma senha, ou ate mesmo outra coisa, criptografa e devolve qualquer entrada criptografada.
 
     cripografada = hashlib.sha256(senha.encode('utf-8'))
-    senha_criptografada = cripografada.hexdigest()
-    return senha_criptografada
+    criptografada = cripografada.hexdigest()
+    return criptografada
+
+def speed_cast(arquivo, adm):
+    dados = leitura_json(arquivo)
+    caminho = procura_caminho(arquivo)
+
+    usuario = input('Informe o nome do usuario: ')
+    usuario = remove_espaco(usuario)
+
+    senha = input('Informe a senha do usuario: ')
+    senha = remove_espaco(senha)
+    senha = criptografa(senha)
+
+    for dado in dados:
+        if dado['login'] == usuario:
+            print('este usuario ja existe.')
+
+    novo_usuario = {'login': usuario, 'senha': senha, 'adm': adm}
+    dados.append(novo_usuario)
+
+    with open(caminho, 'w', encoding='utf-8' ) as folder:
+        json.dump(dados, folder, indent=4)
+
+    return usuario
 
 def listar_usuarios(menu):
     #/ Pega usuario por usuario e imprime na tela.
@@ -51,11 +132,6 @@ def limpar_tela():
     os.system('cls')
     print('--------------------------------------------------')
 
-def voltar_menu(menu):
-    print('--------------------------------------------------')
-    input('Aperte ENTER para retornar ao menu.')
-    print('--------------------------------------------------')
-    menu()
 
 def fechar():
 
