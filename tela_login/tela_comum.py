826d8b83cf6661d4b503b4ee880db8effc9e07a8
Tomaz Roberto <69155134+tomazrgs@users.noreply.github.com>
2026-01-05 11:03:14 -0300

    Update tela_comum.py
    Adicionado manipulação json com verificações mais seguras e cadastro de pergunta secreta para recuperação de senha do usuario

--- tela_login/tela_comum.py
+++ tela_login/tela_comum.py
@@ -1,7 +1,12 @@
 import time
 import json
 
-from utils import criptografa_senha
+from tela_adm import opcoes_adm
+
+from interface import menu
+from interface import erro_login_cad
+
+from utils import criptografa
 from utils import limpar_tela
 from utils import voltar_menu
 from utils import fechar
@@ -10,9 +15,12 @@ from utils import remove_espaco
 from utils import leitura_json
 from utils import copiar_json
 from utils import procura_caminho
+from utils import trocar_senha_validacao
 
 def entrar():
     x = 0
+
+    usuario_encontrado = None
 
     while x < 3:
         usuario_encontrado = False
@@ -26,32 +34,43 @@ def entrar():
             usuario = remove_espaco(usuario)
 
         senha = input(f'Olá {usuario} informe sua senha: ')
-        senha_criptografada = criptografa_senha(senha)
+        senha_criptografada = criptografa(senha)
 
         for dado in dados:
             if dado['login'] == usuario and dado['senha'] == senha_criptografada:
                 usuario_encontrado = True
-                print('logado com sucesso')
-                break
+                if dado['adm']:
+                    opcoes_adm()
+                else: print('logado com sucesso')
 
         if not usuario_encontrado:
             x += 1
-            print('Usuario ou senha invalidos, por favor tente novamente:')
-            time.sleep(1)
+            erro_login_cad()
+        elif usuario_encontrado:
+            break
             
-
     if x == 3:
+        limpar_tela()
+        dados = leitura_json('dados.json')
+
+
         print('Limite de tentativas excedidas, tente novamente.')
-        time.sleep(2)
-        voltar_menu(menu)
+        escolha = input('Caso tenha esquecido sua senha digite "SIM": ').lower()
+
+        if escolha == 'sim':
+
+            trocar_senha_validacao()
+            time.sleep(1)
+            voltar_menu(opcoes)
 
 def cadastrar():
     dados = leitura_json('dados.json')
     copiar_json('dados.json', 'backup.json')
 
-    i = 0 
+    i = 0  
 
-    usuario = input('Escolha um nome de usuario: \n')
+    limpar_tela()
+    usuario = input('Escolha um nome de usuario: \n').lower()
     usuario = remove_espaco(usuario)
     endereco = procura_caminho('dados.json')
 
@@ -68,17 +87,16 @@ def cadastrar():
     senha = remove_espaco(senha) 
 
     while senha == '': 
-
         print('Senha invalida por favor tente novamente\n')
         senha = input('Crie uma senha: ')
         senha = remove_espaco(senha)
 
-    senha_criptografada = criptografa_senha(senha)
+    senha_criptografada = criptografa(senha)
 
     repet_senha = input('Digite a senha novamente:')
     repet_senha = remove_espaco(senha)
 
-    y =0
+    y = 0
 
     while repet_senha != senha and y < 3:
         y += 1
@@ -87,8 +105,13 @@ def cadastrar():
     if y == 3:
         print('tentativas excedidas, por favor tente novamente')
         time.sleep(2)
+        opcoes()
 
-    novo_usuario = {'login': usuario , 'senha': senha_criptografada, 'adm': False}
+    pergunta = input('Escolha uma pergunta de segurança: ')
+    resposta = input('Qual a resposta: ').lower()
+    resposta = criptografa(resposta)
+
+    novo_usuario = {'login': usuario , 'senha': senha_criptografada, 'adm': False, 'pergunta': pergunta, 'resposta': resposta}
 
     with open(endereco, 'w', encoding='utf-8') as file:
         dados.append(novo_usuario)
@@ -96,20 +119,11 @@ def cadastrar():
 
     print(f'{usuario} seu cadastro foi feito com sucesso!! ')
     time.sleep(2)
-    voltar_menu(menu)
+    voltar_menu(opcoes)
 
-def menu():
-
-    limpar_tela()
-    print('\n1: Entrar ' 
-    '\n2: Cadastrar' 
-    '\n3: Lista de usuários'
-    '\n4: Sair') 
-    print('--------------------------------------------------')
-    opcoes()
 
 def opcoes(): 
-
+    menu()
     #/ lieralmente switch case do C
 
     escolha = int(input('\nEscolha uma opção '))
@@ -124,4 +138,4 @@ def opcoes():
             fechar()
 
 if __name__ == '__main__':
-    menu()
+    opcoes()
