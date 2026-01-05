05d4c5f0b143c678f6e5dc8f7b412ea7684da618
Tomaz Roberto <69155134+tomazrgs@users.noreply.github.com>
2026-01-05 11:09:26 -0300

    Update interface.py
    Adicionado arquivo apenas para ter uma separação decente do que é executável e o que é apenas escrita, separando de forma eficiente a interface do código

--- tela_login/interface.py
+++ tela_login/interface.py
@@ -1,1 +1,36 @@
+import time
 
+
+from utils import limpar_tela
+
+
+def voltar_menu(menu):
+    print('--------------------------------------------------')
+    input('Aperte ENTER para retornar ao menu.')
+    print('--------------------------------------------------')
+    menu()
+
+def menu():
+
+    limpar_tela()
+    print('\n1: Entrar ' 
+    '\n2: Cadastrar' 
+    '\n3: Lista de usuários'
+    '\n4: Sair') 
+    print('--------------------------------------------------')
+
+def menu_admin():
+
+    limpar_tela()
+    print('--bem vindo ao menu de adm--')
+    
+    print('\n1: Cadastrar outro adm ' 
+    '\n2: Trocar senha de usuario' 
+    '\n3: Remover Usuario'
+    '\n4: Listar usuários'
+    '\n5: Sair') 
+    print('--------------------------------------------------')
+
+def erro_login_cad():
+    print('Usuario ou senha invalidos por favor tete novamente.')
+    time.sleep(2)
