4bc98d609e026288af84170d0fc1e585a5fe962a
Tomaz Roberto <69155134+tomazrgs@users.noreply.github.com>
2026-01-13 16:28:27 -0300

    Update tela_adm.py
    Adicionado log

--- tela_login/tela_adm.py
+++ tela_login/tela_adm.py
@@ -1,9 +1,12 @@
 import time
 import json
+import logging
+
+from log_config import get_logger
 
 from interface import menu_admin
+from interface import erro_arquivo
 
-from utils import criptografa
 from utils import limpar_tela
 from utils import listar_usuarios
 from utils import fechar
@@ -15,47 +18,71 @@ from utils import procura_caminho
 from utils import speed_cast
 from utils import trocar_senha_adm
 
+logger = get_logger(__name__)
+
+def promocao_adm(user):
+    caminho = procura_caminho('dados,json')
+    dados = leitura_json('dados.json')
+    novo_adm = input ('Informe o nome do usuario: ')
+    novo_adm = remove_espaco(novo_adm)
     
-def novo_admin():
-    encontrado = None
+
+    for dado in dados:
+        if dado['login'] == novo_adm and dado['adm'] == False:
+            dado['adm'] = True
+
+            try:
+                with open(caminho,'w', encoding='utf-8') as arquivo:
+                    json.dump(dados,arquivo, indent=4)
+            except:
+                erro_arquivo()
+                logger.error('Arquivo não esta conseguindo ser leito.')
+                opcoes_adm()
+
+            print(f'{novo_adm} agora é um adm')
+            logger.info(f'{user} promoveu {novo_adm} a ADM.')
+
+        elif dado['login'] == novo_adm and dado['adm']:
+            print('usuario já é um adm')
+            logger.warning(f'{user} tentou promover {novo_adm} mas já é um ADM.')
+            time.sleep(2)
+            opcoes_adm()
+
+def cad_adm(user):
     caminho = procura_caminho('dados.json')
-    limpar_tela()
+    novo_adm = speed_cast('dados.json', True)
+    dados = leitura_json('dados.json')
+    logger.info(f'{user} realizou o cadastro de {novo_adm}')
 
-    print('1: Usuario existente\n' \
-        '2: Usuario não existente\n')
+    try:
+        with open(caminho,'w', encoding='utf-8') as arquivo:
+            json.dump(dados,arquivo, indent=4)
+    except:
+        erro_arquivo()
+        logger.error('Arquivo impossibilitado de abrir ou ser achado.')
+        opcoes_adm()
 
-    escolha = int(input('Escolha uma opção: '))
+    print(f'{novo_adm} cadastrado como adm')
+    opcoes_adm(user)
+
+def opcao_novo_adm(escolha, user):
 
     match escolha:
         case 1:
-            dados = leitura_json('dados.json')
-            novo_adm = input ('Informe o nome do usuario: ')
-            novo_adm = remove_espaco(novo_adm)
-
-            for dado in dados:
-                if dado['login'] == novo_adm and dado['adm'] == False:
-                    encontrado = True
-                    dado['adm'] = True
-                    print(f'{novo_adm} agora é um adm')
-
-                elif dado['login'] == novo_adm and dado['adm']:
-                    print('usuario já é um adm')
-                    time.sleep(2)
-                    opcoes_adm()
+            promocao_adm(user)
         case 2:
-            novo_adm = speed_cast('dados.json', False)
-            dados = leitura_json('dados.json')
-            for dado in dados:
-                if dado['login'] == novo_adm and dado['adm'] == False:
-                    encontrado = True
-                    dado['adm'] = True
-                    print(f'{novo_adm} cadastrado como adm')
+            cad_adm(user)
 
-    if encontrado:
-        with open(caminho,'w', encoding='utf-8') as arquivo:
-            json.dump(dados,arquivo, indent=4)
+def novo_admin(user):
 
-def deletar_usuario():
+    limpar_tela()
+
+    print('1: Usuario existente\n' \
+          '2: Usuario não existente\n')
+    escolha = int(input('Escolha uma opção: '))
+    opcao_novo_adm(escolha, user)
+
+def deletar_usuario(user):
     dados = leitura_json('dados.json')
     copiar_json('dados.json', 'backup.json')
     caminho = procura_caminho('dados.json')
@@ -69,20 +96,22 @@ def deletar_usuario():
     with open(caminho, 'w', encoding='utf') as folder:
         json.dump(dados,folder, indent=4)
 
-def opcoes_adm():
+    logger.info(f'O ADM {user} deletou a conta de {usuario}')
+
+def opcoes_adm(user):
     #/ lieralmente switch case do C
     menu_admin()
 
     escolha = int(input('\nEscolha uma opção '))
     match escolha:
         case 1: # Chama a função entrar
-            novo_admin()
+            novo_admin(user)
         case 2:
-            trocar_senha_adm()
+            trocar_senha_adm(user)
         case 3:
-            deletar_usuario()
+            deletar_usuario(user)
         case 4:
-            listar_usuarios(opcoes_adm)
+            listar_usuarios(opcoes_adm, user)
         case 5:
             fechar()
 
