01301520c46d93c6e469d66b3654fa75fc9e3027
Tomaz Roberto <69155134+tomazrgs@users.noreply.github.com>
2026-01-13 16:27:45 -0300

    Update tela_comum.py
    Adicionado log
    
    Adicionado em "entrar" opção de primeiro acesso para adicionar pergunta secreta
    
    Adicionado em "entrar" modelo novo do banco

--- tela_login/tela_comum.py
+++ tela_login/tela_comum.py
@@ -1,12 +1,18 @@
 import time
 import json
+import logging
+
+from log_config import get_logger
 
 from tela_adm import opcoes_adm
 
-from interface import erro_arquivo
 from interface import menu
 from interface import erro_login_cad
+from interface import erro_arquivo
+from interface import stile
 
+from utils import increment_pergunta
+from utils import gera_id
 from utils import criptografa
 from utils import limpar_tela
 from utils import fechar
@@ -16,55 +22,67 @@ from utils import leitura_json
 from utils import copiar_json
 from utils import procura_caminho
 from utils import trocar_senha_validacao
+from utils import verifica_vazio
+
+logger = get_logger(__name__)
+
 
 def entrar():
+
+    global logger
+
     x = 0
 
-    usuario_encontrado = None
+    usuario_encontrado = False
 
-    while x < 3:
-        usuario_encontrado = False
+    while x < 3 or not usuario_encontrado:
+
         try:
             dados = leitura_json('dados.json')
         except:
             erro_arquivo()
+            logger.error('Arquivo não esta conseguindo ser leito.')
             opcoes()
-            
+
         usuario = input('Informe seu usuario: ').lower() 
         usuario = remove_espaco(usuario)
 
-        while usuario == '':
-            usuario = input('Escreva nome de usuario: ')
-            usuario = remove_espaco(usuario)
+        usuario = verifica_vazio(usuario, 'usuario')
 
         senha = input(f'Olá {usuario} informe sua senha: ')
+        senha = remove_espaco(senha)
+
+        senha = verifica_vazio(senha, 'senha')
+
         senha_criptografada = criptografa(senha)
 
         for dado in dados:
-            if dado['login'] == usuario and dado['senha'] == senha_criptografada:
+            if dado['login'] == usuario and dado['senha'] == senha_criptografada and not dado['primeiro acesso']:
                 usuario_encontrado = True
                 if dado['adm']:
-                    opcoes_adm()
-                else: print('logado com sucesso')
+                    logger.info(f'{usuario} foi logado como adm.')
+                    opcoes_adm(usuario)
+                else: print('logado com sucesso'), logger.info(f'{usuario} logado com sucesso')
+            elif dado['login'] == usuario and dado['primeiro acesso']:
+                usuario_encontrado = True
+                increment_pergunta(usuario)
+                opcoes()
 
         if not usuario_encontrado:
             x += 1
+            logger.warning('tentativa de login, usuario nao encontrado')
             erro_login_cad()
         elif usuario_encontrado:
             break
             
     if x == 3:
         limpar_tela()
-        try:
-            dados = leitura_json('dados.json')
-        except:
-            erro_arquivo()
-            opcoes()
 
-        print('Limite de tentativas excedidas, tente novamente.')
+        stile('Limite de tentativas excedidas, tente novamente.')
         escolha = input('Caso tenha esquecido sua senha digite "SIM": ').lower()
 
         if escolha == 'sim':
+            logger.info('Reset de senha requisitado.')
             trocar_senha_validacao()
             time.sleep(1)
             opcoes()
@@ -73,6 +91,7 @@ def cadastrar():
     try:
         dados = leitura_json('dados.json')
     except:
+        logger.error('Arquivo não esta conseguindo ser leito.')
         erro_arquivo()
         opcoes()
         
@@ -91,6 +110,7 @@ def cadastrar():
             i += 1
             if i == 3:
                 print('Tentativas excedidas, por favor tente novamente do menu')
+                logger.warning('tentativas de cadastro excedidas.')
                 time.sleep(2)
                 opcoes()
 
@@ -115,6 +135,7 @@ def cadastrar():
         repet_senha = remove_espaco(senha)
     if y == 3:
         print('tentativas excedidas, por favor tente novamente')
+        logger.warning('tentativas de cadastro excedidas.')
         time.sleep(2)
         opcoes()
 
@@ -122,14 +143,24 @@ def cadastrar():
     resposta = input('Qual a resposta: ').lower()
     resposta = criptografa(resposta)
 
-    novo_usuario = {'login': usuario , 'senha': senha_criptografada, 'adm': False, 'pergunta': pergunta, 'resposta': resposta}
+    identifica = gera_id()
+
+    novo_usuario = {
+        'ID': identifica , 
+        'login': usuario , 
+        'senha': senha_criptografada, 
+        'adm': False, 
+        'primeiro acesso': False , 
+        'pergunta': pergunta, 
+        'resposta': resposta
+        }
+    dados.append(novo_usuario)
 
     with open(endereco, 'w', encoding='utf-8') as file:
-        dados.append(novo_usuario)
         json.dump(dados, file, indent=4)
-  
 
     print(f'{usuario} seu cadastro foi feito com sucesso!! ')
+    logger.info(f'Novo usuario {usuario} cadastrado.')
     time.sleep(2)
     opcoes()
 
@@ -137,7 +168,7 @@ def opcoes():
     menu()
     #/ lieralmente switch case do C
 
-    escolha = int(input('\nEscolha uma opção '))
+    escolha = int(input('\nEscolha uma opção: '))
     match escolha:
         case 1: # Chama a função entrar
             entrar()
