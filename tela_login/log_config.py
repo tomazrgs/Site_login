1ae293733fd3a13dd949ce86c9bd73f9c0dffa63
Tomaz Roberto <69155134+tomazrgs@users.noreply.github.com>
2026-06-22 07:12:56 -0300

    Update log_config.py
    Atualizado para o arquivo definitivo que residia em outro computador sem commit a meses.

--- tela_login/log_config.py
+++ tela_login/log_config.py
@@ -1,6 +1,11 @@
 import logging
+import os
 
-logging.basicConfig(filename='app.log',encoding='utf-8', level=logging.DEBUG, format='%(name)s %(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
+log_dir = "data"
+log_file = os.path.join(log_dir, "app.log")
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(filename= log_file,encoding='utf-8', level=logging.DEBUG, format='%(name)s %(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
 
 def get_logger(name):
     return logging.getLogger(name)
